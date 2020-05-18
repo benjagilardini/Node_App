92cf065a611f4ead544e417457b38768bf3615e1
Sencha <benja.gilardini@gmail.com>
2020-05-18 16:44:32 -0300

    Ejercicios de practica 18/05

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-const logger = require('./logger');
-const path = require('path');
-const os = require('os');
 const fs = require('fs');
 
-let pathObj = path.parse(__filename);
-let dir = pathObj.dir;
-logger.log(pathObj);
-console.log(dir);
+//4 Tareas 
+//1-Create a method to get all the files inside given path.
+let rootDir = fs.readdirSync('/home/benjag/Documentos/1asd/');
 
-let freeMemory = os.freemem();
-let totalMemory = os.totalmem();
-logger.log('Free Memory: ', freeMemory);
-logger.log('Total Memory: ', totalMemory);
+//2-create a method to print all the files.
+fs.readdir('/home/benjag/Documentos/1asd/', (err, files) => {
+    (err) ? console.log(err) : console.log('Files: ', files);
+});
 
-let rootDir = fs.readdirSync('./');
+// //3-create a method to sort and  print the files.
+fs.readdir('/home/benjag/Documentos/1asd/', (err, files) => {
+    (err) ? console.log(err) : console.log('Sorted_Files:', files.reverse());
+});
 
-fs.readdir('./', (err, files) => {
-    (err) ? console.log(err) : console.log('Async:', files);
-})
-console.log('Sync:' ,rootDir);
+// 4-create a method to get the amount of files starting with one letter.
+fs.readdir('/home/benjag/Documentos/1asd/', (err, files) => {
+    let count = 0;
+    let letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    files.forEach(file => {
+        letter.forEach(letra => {
+            if (file.startsWith(letra)) {
+                count++;
+            }
+        })
+    });
+    (err) ? console.log(err) : console.log('Files_start_with_letter:', count);
+});
