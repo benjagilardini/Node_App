704d39bc6085a6fd1b5bd7e3f1e7b4c9f3810279
Sencha <benja.gilardini@gmail.com>
2020-05-19 10:46:37 -0300

    Agrego alternativa de solucion a la consigna 4

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const logger = require('./logger');
 
 //4 Tareas 
 //1-Create a method to get all the files inside given path.
@@ -14,7 +15,9 @@ fs.readdir('/home/benjag/Documentos/1asd/', (err, files) => {
     (err) ? console.log(err) : console.log('Sorted_Files:', files.reverse());
 });
 
-// 4-create a method to get the amount of files starting with one letter.
+// // 4-create a method to get the amount of files starting with one letter.
+
+//OPCION 1:
 fs.readdir('/home/benjag/Documentos/1asd/', (err, files) => {
     let count = 0;
     let letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -26,4 +29,15 @@ fs.readdir('/home/benjag/Documentos/1asd/', (err, files) => {
         })
     });
     (err) ? console.log(err) : console.log('Files_start_with_letter:', count);
+});
+//OPCION 2:
+fs.readdir('/home/benjag/Documentos/1asd/', (err, files) => {
+    let count = 0;
+    let letter = 'a';
+    files.forEach(file => {
+        if (file.startsWith(letter)) {
+            count++;
+        }
+    });
+    (err) ? logger.log(err) : logger.log('Files_start_with_letter:', count);
 });
